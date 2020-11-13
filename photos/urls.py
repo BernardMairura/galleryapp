600bac398ddd66c8063648e66c95b3b0ee23f266
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-11-13 16:52:43 +0300

    templates for display

--- photos/urls.py
+++ photos/urls.py
@@ -3,6 +3,8 @@ from .import views
 
 urlpatterns=[
     path('^$',views.welcome,name='welcome'),
+    # path('^today/$',views.photos_of_day,name='newsToday'),
+    # re-path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.previous_days_photos,name = 'previousPhotos'),
 ]
 
 
