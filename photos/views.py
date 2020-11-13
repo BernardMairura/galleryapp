c2812e6476f05b46503b431708f07215e91a5555
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-11-13 17:56:11 +0300

    create welcome render template

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('WELCOME VIEWER')
+    return render(request, 'welcome.html')
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+def photos_of_day(request):
+    date = dt.date.today()
+    return render(request, 'all-photos/today-photos.html', {"date": date,})
+
+# View Function to present photos from the previous days
+def previous_days_photos(request, previous_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(previous_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(photos_of_day)
+
+    return render(request, 'all_photos/previous_news.html', {"date": date})
