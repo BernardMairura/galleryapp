78f8d3007806b88d9165a8dc2549971325039dd2
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-11-15 14:26:25 +0300

    setting configuration parameters globally in your Django application

--- gallery/settings.py
+++ gallery/settings.py
@@ -187,3 +187,10 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+#setting configuration parameters globally in your Django application:
+cloudinary.config( 
+  cloud_name = "sample", 
+  api_key = "874837483274837", 
+  api_secret = "a676b67565c6767a6767d6767f676fe1" 
+)
