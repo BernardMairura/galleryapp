7a20219a2fa0c92732dd5094cbb8088912a1e94f
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-11-16 07:45:59 +0300

    cloudinary changes

--- photos/urls.py
+++ photos/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 
 urlpatterns=[
-    re_path(r'^$', views.my_images, name='my_images'),
+    path('', views.my_images, name='my_images'),
     
     path('search/', views.search_results, name='search_results'),
     re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.previous_days_photos,name = 'previousPhotos'),
